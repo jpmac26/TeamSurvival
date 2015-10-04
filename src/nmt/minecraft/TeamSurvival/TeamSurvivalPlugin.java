edd932ae29f28784b1e272f8aef3fd0706d8c140
Skyler Manzanares <skymanzanares@hotmail.com>
2015-10-04 13:44:52 -0600

    Set up git ignore and a basic package

---
+++ src/nmt/minecraft/TeamSurvival/TeamSurvivalPlugin.java
@@ -0,0 +1,29 @@
+package nmt.minecraft.TeamSurvival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Plugin class.<br />
+ * Creates everything and all things and everything.
+ * @author Skyler
+ *
+ */
+public class TeamSurvivalPlugin extends JavaPlugin {
+	
+	
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	@Override
+	public void onLoad() {
+		
+	}
+}
