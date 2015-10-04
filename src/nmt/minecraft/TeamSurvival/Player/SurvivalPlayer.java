98b13f5f5bc105c3d639f1e83a8b530fab5c7d48
Skyler Manzanares <skymanzanares@hotmail.com>
2015-10-04 15:31:48 -0600

    Added player section

---
+++ src/nmt/minecraft/TeamSurvival/Player/SurvivalPlayer.java
@@ -0,0 +1,55 @@
+package nmt.minecraft.TeamSurvival.Player;
+
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+
+/**
+ * A wrapper class for someone playing the game.<br />
+ * @author Skyler
+ *
+ */
+public class SurvivalPlayer {
+	
+	/**
+	 * The UUID of the player we're following
+	 */
+	private UUID playerID;
+	
+	/**
+	 * Creates a new wrapper survival player.<br />
+	 * Since we only want one survival player per player, we make this private and use static getter
+	 * @param player The player to wrap around
+	 */
+	private SurvivalPlayer(OfflinePlayer player) {
+		; //TODO
+	}
+	
+	/**
+	 * @see Team#win()
+	 */
+	protected void win() {
+		; //TODO
+	}
+	
+	/**
+	 * @see Team#lose()
+	 */
+	protected void lose() {
+		; //TODO
+	}
+	
+	
+	/**
+	 * Looks up and returns the player wrapped by this object.<br />
+	 * We have to do it this way incase someone logs off, then back in; their 'Player' id/reference to their Player is now broken.
+	 * @return The OfflinePlayer associated with this player
+	 * @note don't forget to make sure the player's online
+	 * @see OfflinePlayer
+	 */
+	public OfflinePlayer getOfflinePlayer() {
+		return Bukkit.getOfflinePlayer(playerID);
+	}
+	
+}
---
+++ src/nmt/minecraft/TeamSurvival/Player/Team.java
@@ -0,0 +1,64 @@
+package nmt.minecraft.TeamSurvival.Player;
+
+import java.util.Collection;
+
+import org.bukkit.Location;
+
+/**
+ * A group of players.<br />
+ * Teams are the the big operational block for the user part of our game. We rarely will operate on single players;
+ * instead we'll use teams.
+ * @author Skyler
+ */
+public class Team {
+	
+	/**
+	 * Collection of the players that are part of this team
+	 */
+	private Collection<SurvivalPlayer> players;
+	
+	/**
+	 * This team's name
+	 */
+	private String name;
+	
+	public Team(String name, Collection<SurvivalPlayer> players) {
+		; //TODO
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void addPlayer(SurvivalPlayer player) {
+		players.add(player);
+	}
+	
+	/**
+	 * Mainly a glamour function. Does whatever we want the players see/experience when they win, like
+	 * play a sound and display something in chat, etc. Lots of room for creativity here.<br />
+	 * Should also return players to lobby, etc
+	 */
+	public void win() {
+		; //TODO
+	}
+	
+	/**
+	 * The counterpart to win.
+	 * @see #win()
+	 */
+	public void lose() {
+		; //TODO
+	}
+	
+	/**
+	 * Moves a whole team to a specific location.<br />
+	 * This can be the shop area, or the arena they're gonna fight monsters on
+	 * @param location
+	 */
+	public void moveTo(Location location) {
+		; //TODO
+	}
+	
+	
+}
