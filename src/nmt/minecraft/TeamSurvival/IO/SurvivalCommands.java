5207ceab54ef52f65832bc44e4297d5bf08b2191
Skyler Manzanares <skymanzanares@hotmail.com>
2015-10-06 17:24:20 -0600

    Separated out commands to different methods

--- src/nmt/minecraft/TeamSurvival/IO/SurvivalCommands.java
+++ src/nmt/minecraft/TeamSurvival/IO/SurvivalCommands.java
@@ -9,21 +9,56 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class SurvivalCommands implements CommandExecutor {
-	private static String[] commandList = {"teamsurvival", "jointeam"};
+	
+	/**
+	 * The plugin's commands, handled by this executor
+	 */
+	private static final String[] commandList = {"teamsurvival", "jointeam"};
+	
+	/**
+	 * A list of commands handled by the 'team survival' wrapping command
+	 */
+	private static final String[] subCommandList = {"session", "team"};
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (!(sender instanceof Player)) {
-			sender.sendMessage("Only players may use this command.");
-			return false;
+		
+		if (cmd.getName().equalsIgnoreCase("teamsurvival")) {
+			onTeamSurvivalCommand(sender, args);
+			return true;
 		}
 		
-		//TODO
+		if (cmd.getName().equalsIgnoreCase("jointeam")) {
+			onJoinCommand(sender, args);
+		}
 		
-		return true;
+		return false;
 	}
 	
 	public static List<String> getCommandList(){
 		return Arrays.asList(commandList);
+	}
+	
+	/**
+	 * Handles a join command
+	 * @param sender Who send the command
+	 * @param args The args passed to the command
+	 */
+	private void onJoinCommand(CommandSender sender, String[] args) {
+		if (!(sender instanceof Player)) {
+			sender.sendMessage("Only players can use this command!");
+			return;
+		}
+		
+		; //TODO
+	}
+	
+	/**
+	 * Handles the admin-command wrapper
+	 * @param sender Who sent the command
+	 * @param args The arguments they supplied
+	 */
+	private void onTeamSurvivalCommand(CommandSender sender, String[] args) {
+		
 	}
 }
