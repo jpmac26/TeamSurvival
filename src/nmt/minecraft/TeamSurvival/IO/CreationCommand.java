82fb1787cb85e21635978310b0067341b4e56fbb
Stephanie Martinez <smarti02@nmt.edu>
2015-10-06 21:50:56 -0600

    creation tab completer done

--- src/nmt/minecraft/TeamSurvival/IO/CreationCommand.java
+++ src/nmt/minecraft/TeamSurvival/IO/CreationCommand.java
@@ -1,15 +1,24 @@
 package nmt.minecraft.TeamSurvival.IO;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class CreationCommand implements CommandExecutor{
-
+	
+	private static final String[] commands = {"new", "open", "close", "setShop", "addArena"};
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	public static List<String> getCommands(){
+		return Arrays.asList(commands);
 	}
 
 }
--- src/nmt/minecraft/TeamSurvival/IO/CreationTabCompleter.java
+++ src/nmt/minecraft/TeamSurvival/IO/CreationTabCompleter.java
@@ -1,18 +1,44 @@
 package nmt.minecraft.TeamSurvival.IO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
+import nmt.minecraft.TeamSurvival.TeamSurvivalPlugin;
+
 public class CreationTabCompleter implements TabCompleter{
 
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-		// TODO Auto-generated method stub
+		if(args.length == 1){
+			return getBeginList(args[0], CreationCommand.getCommands());
+		}
+		
+		if(args.length == 2 && (args[0].equals("open")||args[0].equals("close"))){
+			//TODO get a list from static Map call
+		}
+
 		return null;
 	}
 	
-
+	private List<String> getBeginList(String key, List<String> totalList){
+		List<String> list = new ArrayList<String>();
+		
+		for(String s : totalList){
+			if(startsWithIgnoreCase(s,key)){
+				list.add(s);
+			}
+		}
+		
+		return list;
+	}
+	
+	private static boolean startsWithIgnoreCase(String string1, String string2){
+		string1 = string1.toLowerCase();
+		string2 = string2.toLowerCase();
+		return string1.startsWith(string2);
+	}
 }
