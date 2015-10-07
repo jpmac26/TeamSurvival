89126f5214c6c3ca1f40309b78567b18dc26782d
Stephanie Martinez <smarti02@nmt.edu>
2015-10-07 00:53:20 -0600

    did implementation for creation commands

--- src/nmt/minecraft/TeamSurvival/Map/Creator.java
+++ src/nmt/minecraft/TeamSurvival/Map/Creator.java
@@ -1,26 +1,31 @@
 package nmt.minecraft.TeamSurvival.Map;
 
 import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import nmt.minecraft.TeamSurvival.IO.ChatFormat;
 
-public class Creator {
+public final class Creator {
 	private static Map current = null;
 	
-	public static boolean open(String name){
+	public static boolean open(CommandSender sender,String name){
 		if(current != null){
-			ChatFormat.ERROR.wrap("There is already an open session");
+			sender.sendMessage(ChatFormat.ERROR.wrap("There is already an open session"));
 			return false;
 		}
 		
-		Creator.current = new Map(name);
-		
+		if(Map.listConfigs().contains(name)){
+			//TODO: call Map's load config for the file
+		}else{
+			Creator.current = new Map(name);
+		}
 		return true;
 	}
 	
-	public static boolean close(){
+	public static boolean close(CommandSender sender){
 		if(current == null){
-			ChatFormat.WARNING.wrap("There is no open session");
+			sender.sendMessage(ChatFormat.WARNING.wrap("There is no open session"));
 			return false;
 		}
 		
@@ -29,38 +34,58 @@ public class Creator {
 		return true;
 	}
 	
-	public static boolean addArena(Location location){
+	public static boolean addArena(CommandSender sender){
 		if(current == null){
-			ChatFormat.ERROR.wrap("There is no open session");
+			sender.sendMessage(ChatFormat.ERROR.wrap("There is no open session"));
 			return false;
 		}
 		
-		Creator.current.addArenaLocation(location);
+		if(sender instanceof Player){
+			Creator.current.addArenaLocation(((Player)sender).getLocation());
+		}else{
+			sender.sendMessage(ChatFormat.ERROR.wrap("You need to be a player!"));
+			sender.sendMessage("Set added arena at: "+Creator.getLocation(((Player)sender).getLocation()));
+		}
+		return true;
+	}
+	
+	public static boolean setShopLocation(CommandSender sender){
+		if(current == null){
+			sender.sendMessage(ChatFormat.ERROR.wrap("There is no open session"));
+			return false;
+		}
+		
+		if(sender instanceof Player){
+			Creator.current.setShopLocation(((Player)sender).getLocation());
+			sender.sendMessage("Set shop location to: "+Creator.getLocation(((Player)sender).getLocation()));
+		}else{
+			sender.sendMessage(ChatFormat.ERROR.wrap("You need to be a player!"));
+		}
 		
 		return true;
 	}
 	
-	public static boolean setShopLocation(Location location){
+	public static boolean setShopButton(CommandSender sender){
 		if(current == null){
 			ChatFormat.ERROR.wrap("There is no open session");
 			return false;
 		}
-		
-		Creator.current.setShopLocation(location);
-		
-		return true;
-	}
-	
-	public static boolean setShopButton(Location location){
-		if(current == null){
-			ChatFormat.ERROR.wrap("There is no open session");
-			return false;
+		if(sender instanceof Player){
+			Creator.current.setShopButton(((Player)sender).getLocation());
+			sender.sendMessage("Set button to: "+Creator.getLocation(((Player)sender).getLocation()));
+		}else{
+			sender.sendMessage(ChatFormat.ERROR.wrap("You need to be a player!"));
 		}
 		
-		Creator.current.setShopButton(location);
-		
 		return true;
 	}
 	
+	private static String getLocation(Location location){
+		String loc = "";
+		loc += location.getX()+", ";
+		loc += location.getY()+", ";
+		loc += location.getZ()+", ";
+		return loc;
+	}
 	
 }
