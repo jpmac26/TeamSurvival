20a837519c736dd7189c4d2765a750b48c7828c1
Skyler Manzanares <skymanzanares@hotmail.com>
2015-10-06 23:26:43 -0600

    pause

--- src/nmt/minecraft/TeamSurvival/Shop/Shop.java
+++ src/nmt/minecraft/TeamSurvival/Shop/Shop.java
@@ -1,5 +1,16 @@
 package nmt.minecraft.TeamSurvival.Shop;
 
+/**
+ * A shop that offers items to players for a price.<br />
+ * Has a location that it listens for to interact with, and then presents an inventory menu.
+ * @author Skyler
+ *
+ */
 public class Shop {
-
+	
+	private Inventory shopInventory();
+	
+	private Location buttonLocation;
+	
+	
 }
