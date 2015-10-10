9e60225cd82d7ea4462fc77d4c700864ab46f2d6
Stephanie Martinez <smarti02@nmt.edu>
2015-10-09 21:23:27 -0600

    added getNextArena and addArena to Map

--- src/nmt/minecraft/TeamSurvival/Session/GameSession.java
+++ src/nmt/minecraft/TeamSurvival/Session/GameSession.java
@@ -415,6 +415,6 @@ public class GameSession implements Listener, Tickable {
 		
 		int avg = sum/teams.size();
 		
-		return avg + (int)(waveNumber*1.5);
+		return avg + (int)(waveNumber*(avg+5));
 	}
 }
