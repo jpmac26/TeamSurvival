9b162968b6851ffa1ead7100a88f792b2d2272af
Skyler Manzanares <skymanzanares@hotmail.com>
2015-10-09 23:40:08 -0600

    register and unregister teams

--- src/nmt/minecraft/TeamSurvival/Player/Team.java
+++ src/nmt/minecraft/TeamSurvival/Player/Team.java
@@ -12,6 +12,7 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
@@ -146,6 +147,8 @@ public class Team implements Listener {
 		for (SurvivalPlayer p : this.players) {
 			p.lose();
 		}
+		
+		HandlerList.unregisterAll(this);
 		//Teleport all players
 		//this.moveTo(lobbyLocation);
 	}
