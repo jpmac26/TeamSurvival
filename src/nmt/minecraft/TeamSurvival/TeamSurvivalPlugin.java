fe39edbe3dc2163ab4756e6e6c1b183195cb3deb
chavezfk <chavezfk@gmail.com>
2015-10-09 20:18:42 -0600

    removed the commands for testing the mobs part

--- src/nmt/minecraft/TeamSurvival/TeamSurvivalPlugin.java
+++ src/nmt/minecraft/TeamSurvival/TeamSurvivalPlugin.java
@@ -31,7 +31,7 @@ public class TeamSurvivalPlugin extends JavaPlugin implements Listener {
 	
 	@Override
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(this, this);
+		
 	}
 	
 	@Override
@@ -50,62 +50,6 @@ public class TeamSurvivalPlugin extends JavaPlugin implements Listener {
 	private void triggerWaveNewSpawnCheck(EntityDeathEvent event) {
 		for(Wave foundWave : Waves) {
 			foundWave.onEntityDeath(event);
-		}
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		try {
-			if (!(sender instanceof Player)) {
-				sender.sendMessage("Only players may use this command.");
-				return false;
-			}
-			
-			//NOTE: These commands are just for testing purposes, this is not how we want to position the spawned mobs
-			//when we actually implement this plugin, and we also probably don't want 100 mobs in each wave,
-			//or even to have 40 spawned at once, because that's gonna be way too hard.
-			if (cmd.getName().equalsIgnoreCase("startTestWave")) {
-				List<Location> spawnPoints = new ArrayList<Location>();
-				spawnPoints.add(new Location(((Player)sender).getLocation().getWorld(), 0.0, 70.0, 0.0));
-				spawnPoints.add(new Location(((Player)sender).getLocation().getWorld(), 20.0, 70.0, 0.0));
-				spawnPoints.add(new Location(((Player)sender).getLocation().getWorld(), 0.0, 70.0, 20.0));
-				spawnPoints.add(new Location(((Player)sender).getLocation().getWorld(), -20.0, 70.0, 0.0));
-				spawnPoints.add(new Location(((Player)sender).getLocation().getWorld(), 0.0, 70.0, -20.0));
-				Wave testWave;
-				if(args.length == 0 || args[0].isEmpty() || Integer.parseInt(args[0]) <= 0) {
-					testWave = new Wave(1, spawnPoints, 100);
-				} else {
-					testWave = new Wave(Integer.parseInt(args[0]), spawnPoints, 100);
-				}
-				//Wave testWave = new Wave(1, spawnPoints, 100, 100);
-				testWave.start();
-				Waves.add(testWave);
-			}
-			
-			if (cmd.getName().equalsIgnoreCase("stopWaves")) {
-				for(Wave foundWave : Waves) {
-					foundWave.stop();
-				}
-				Waves.clear();
-			}
-			
-			if (cmd.getName().equalsIgnoreCase("killAllMonsters")) {
-				for(LivingEntity ent : ((Player)sender).getLocation().getWorld().getLivingEntities()) {
-					if(ent.getType() == EntityType.CREEPER | ent.getType() == EntityType.SKELETON
-							| ent.getType() == EntityType.ZOMBIE | ent.getType() == EntityType.ENDERMITE
-							| ent.getType() == EntityType.CREEPER | ent.getType() == EntityType.SPIDER
-							| ent.getType() == EntityType.CAVE_SPIDER) {
-						ent.remove();
-					}
-				}
-			}
-			
-		} 
-		catch (Exception e) {
-			TeamSurvivalPlugin.plugin.getLogger().info("Error: " + e + "\r\n");
-			e.printStackTrace();
-		}
-		
-		return true;
+		}	
 	}
 }
